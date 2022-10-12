f6fb3dbff18d23a4ee6abfba12ab4e2ad313921a
Vijay-VJS1 <109027345+Vijay-VJS1@users.noreply.github.com>
2022-10-12 20:34:09 +0530

    pychachefull

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ st.set_page_config(page_title='👻👻🚗🌫️‍', page_icon='👻',
                    menu_items=None)
 ##################
 page = st.sidebar.selectbox('Select page',['Recommender','Download Data'])
+st.markdown(os.listdir(os.getcwd()))
 st.markdown(os.listdir(f"{os.getcwd()}/pycache"))
+
 if page=='Recommender':
     Recommender()
 elif page=='Download Data':
